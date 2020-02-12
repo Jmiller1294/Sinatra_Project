e9c551645606cd62c66d6d6f07a77b41b4abc4a8
justin <jayman1294@gmail.com>
2020-02-12 15:03:28 -0500

    added routes to games controller and added new and show views

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,2 +1,23 @@
 class GamesController < ApplicationController
+    get "/games/new" do 
+        erb :"/games/new"
+    end
+
+    post "/games" do 
+        @game = Game.create(name: params[:name], console: params[:console], release_date: params[:release_date], details: params[:details])
+        rediret "/games/#{@game.id}"
+    end
+
+    get "/games/:id" do 
+        @game = Game.find_by(id: params[:id])
+        erb :"/games/show"
+    end
+
+    get "/games/:id/edit" do
+        erb :"/games/edit"
+    end
+
+    patch
+    end
+
 end
